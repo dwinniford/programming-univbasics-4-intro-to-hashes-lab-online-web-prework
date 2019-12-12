61ee9053a1d583f882b26b853cd28cded79bd9c7
darling-flag-8189 <dwinniford@gmail.com>
2019-12-12 03:22:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,8 @@ def update_counting_hash(hash, key)
   if hash[key]
     p hash[key] += 1 
   else 
-    hash[key] = 1 
+    hash[key] = 1
+    puts hash[key]
   end 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
