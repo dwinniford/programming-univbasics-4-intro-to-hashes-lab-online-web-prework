419aae97252713981e7e9d061dcd098b06296081
darling-flag-8189 <dwinniford@gmail.com>
2019-12-12 01:55:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,21 @@
-def new_hash
-  # return an empty hash
+def new_hash = {}
 end
 
-def my_hash
-  # return a valid hash with any key/value pair of your choice
+def my_hash = {
+  :monkey = "Banana"
+}
+  
 end
 
-def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+def pioneer = {
+  :name = 'Grace Hopper'
+}
+  
 end
 
-def id_generator
+def id_generator {
+  :id = 6
+}
   # return a hash with a key :id assigned to positive integer
 end
 
